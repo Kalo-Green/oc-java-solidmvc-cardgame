82cf294b0c7ddf8855e9a4b2f30d3b683bbbd502
Kalo-Green <kalogreenstyle@gmail.com>
2026-09-08 07:43:10 +0200

    Add Swing view

--- src/com/openclassrooms/cardgame/games/Games.java
+++ src/com/openclassrooms/cardgame/games/Games.java
@@ -2,12 +2,19 @@ package com.openclassrooms.cardgame.games;
 
 import com.openclassrooms.cardgame.controller.GameController;
 import com.openclassrooms.cardgame.model.Deck;
-import com.openclassrooms.cardgame.view.CommandLineView;
+import com.openclassrooms.cardgame.view.GameSwingView;
 
 public class Games {
 
 	public static void main(String[] args) {
-		GameController gc = new GameController(new Deck(), new CommandLineView(), new HighCardGameEvaluator());
+
+		// Initialise l'interface graphique de l'application.
+		GameSwingView gsv = new GameSwingView();
+		gsv.createAndShowGUI();
+
+		// Configure le Controller avec le paquet de cartes, la View et la règle du jeu.
+		GameController gc = new GameController(new Deck(), gsv, new HighCardGameEvaluator());
+//		GameController gc = new GameController(new Deck(), new CommandLineView(), new HighCardGameEvaluator());
 //		GameController gc = new GameController(new Deck(), new CommandLineView(), new LowCardGameEvaluator());
 		gc.run();
 
